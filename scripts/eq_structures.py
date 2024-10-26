9cec2167eecd9cc6d32a2970c3f6ae05deace665
jancarlo047 <jalvarezc047@outlook.com>
2024-10-25 22:06:36 -0600

    definicion funcion de obtencion id

--- scripts/eq_structures.py
+++ scripts/eq_structures.py
@@ -1,23 +1,39 @@
-def print_menu():
+def print_menu() -> None:
     """
     Esta funcion nos permite mostrar las opciones de modelos
     """
     print("Cual de los siguientes modelos deseas trabajar?")
     print("1.-Isomerizacion")
     print("2.-Dimerizacion")
-    print("3.-Lotka-Volterra")
+    print("3.-Muerte-Nacimiento")
     print("4.-Modelo SIR")
 
-def define_the_structure_function():
-    print_menu()
-    ID_reaction = int(input("Escribe el n'umero identificador del sistema que deseas trabajar:"))
+def obtain_id() -> int:
+
+    while True:
+        ID_reaction = input("Escribe el número identificador del sistema que deseas trabajar: ")
+        try:
+            ID_reaction = int(ID_reaction)
+            print(f"El número identificador es: {ID_reaction}")
+            break  # Salir del bucle si la conversión es exitosa
+        except ValueError:
+            print("Error: Por favor, ingresa un número entero válido.")
+    
     while ID_reaction < 5 & ID_reaction > 0:
             print('El valor proporcionado es erroneo')
             ID_reaction = int(input("Escribe el n'umero identificador del sistema que deseas trabajar:"))
-    reaction = reaction_dynamics(ID_reaction)
-    print(reaction)
-    input()
+    
+    return ID_reaction
 
+def define_the_structure_function():
+    
+    print_menu()
+    
+    ID_reaction = obtain_id()
+
+    reaction = reaction_dynamics(ID_reaction)
+
+    print(reaction)
 
 class reaction_dynamics():
     def __init__(self,id:int):
@@ -28,7 +44,7 @@ class reaction_dynamics():
             1: 'Isomerizacion',
             2: 'Dimerizacion',
             3: 'Lotka-Volterra',
-            4: 'ISR'
+            4: 'SIR'
         }
         self.id = reactions.get(id, 'Desconocido')
 
